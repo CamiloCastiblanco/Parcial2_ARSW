d15f11c092ce48bc2cbcb31c5ce009642feebcef
brayan.castiblanco-b <kmilocastib22@outlook.com>
2023-04-13 07:34:54 -0500

    First

--- src/main/java/edu/eci/arsw/myrestaurant/restcontrollers/OrdersAPIController.java
+++ src/main/java/edu/eci/arsw/myrestaurant/restcontrollers/OrdersAPIController.java
@@ -16,25 +16,74 @@
  */
 package edu.eci.arsw.myrestaurant.restcontrollers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.eci.arsw.myrestaurant.model.Order;
 import edu.eci.arsw.myrestaurant.model.ProductType;
 import edu.eci.arsw.myrestaurant.model.RestaurantProduct;
+import edu.eci.arsw.myrestaurant.services.RestaurantOrderServices;
 import edu.eci.arsw.myrestaurant.services.RestaurantOrderServicesStub;
+import java.lang.ProcessBuilder.Redirect.Type;
+import java.util.Collection;
 import java.util.Hashtable;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.json.JSONException;
+import org.json.JSONObject;
+import com.google.gson.reflect.TypeToken;
+
 
 /**
  *
  * @author hcadavid
  */
+@RestController
+@RequestMapping(value = "/orders")
 public class OrdersAPIController {
+    @Autowired
+    private final RestaurantOrderServicesStub restaurantStub;
+
+    public OrdersAPIController(RestaurantOrderServicesStub restaurantStub){
+        this.restaurantStub = restaurantStub;
+    }
+
+
+    @RequestMapping(method = RequestMethod.DELETE, path = "/{idmesa}")
+    public ResponseEntity<?> deleteOrder(@PathVariable String idmesa){
+        try {
+            restaurantStub.releaseTable(Integer.parseInt(idmesa));
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception ex) {
+            Logger.getLogger(OrdersAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+    }
+    @GetMapping("/{idmesa}")
+    public ResponseEntity getOrder(@PathVariable int idmesa) {
+        try {
+            Map<String, Integer> amounts =  new ConcurrentHashMap();
+            amounts = restaurantStub.getTableOrder(idmesa).getOrderAmountsMap();
+            String json = new ObjectMapper().writeValueAsString(amounts);
+            return  new ResponseEntity<>(json,HttpStatus.OK);
+        } catch (Exception ex) {
+            Logger.getLogger(OrdersAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>(ex.getMessage(),HttpStatus.NOT_FOUND);
+        }
+    }
+
+
 
     
 }
